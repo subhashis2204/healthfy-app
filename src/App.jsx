7700256389807e0b0ff915d622c45ffae2098069
subhashis2204 <subhashispaul2204@gmail.com>
2025-01-22 00:33:57 +0530

    template commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css"
+
+function App() {
+  return (
+    <>
+      <p className="text-2xl">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem assumenda consequuntur autem excepturi sint, cumque
+        nulla, neque quam odit ducimus odio beatae est sed eos quis laborum modi itaque corrupti?
+      </p>
+    </>
+  )
+}
+
+export default App
